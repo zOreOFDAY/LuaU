cacc8b255d15a7c18ccc4f5408464839c88c94ab
zOreOFDAY <cttoreescripter@gmail.com>
2025-03-11 22:15:10 -0300

    Update Save Game.lua

--- Save Game.lua
+++ Save Game.lua
@@ -1,4 +1,7 @@
 -- This script was not made by me but by Universal Syn Save Instance | AGPL-3.0 license
 -- oRee Scripter :D
+print("-- This script was not made by me but by Universal Syn Save Instance | AGPL-3.0 license")
+print("oRee Scripter :D")
+
 
 saveinstance()
